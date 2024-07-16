9ffa53ff93af1239a91c52c77a20cd61a4315840
Adarsh Raveendra Hegde <adarsh.hegde@in.nspglobaltech.com>
2024-07-16 17:31:46 +0530

    Formatting

--- app/app.module.js
+++ app/app.module.js
@@ -1,86 +1,83 @@
-var myApp = angular.module("myModule", ["ngRoute"])
-.config(function ($routeProvider){
+var myApp = angular
+  .module("myModule", ["ngRoute"])
+  .config(function ($routeProvider) {
     $routeProvider
-    .when("/",{
-        templateUrl : "form.html",
-        controller : "MainController"
-    })
-    .when("/table",{
-        templateUrl : "table.html",
-        controller : "MainController"
-    })
-    .otherwise({
-        redirectTo: "/"
-    });
-})
+      .when("/", {
+        templateUrl: "form.html",
+        controller: "MainController",
+      })
+      .when("/table", {
+        templateUrl: "table.html",
+        controller: "MainController",
+      })
+      .otherwise({
+        redirectTo: "/",
+      });
+  })
   .controller("MainController", function ($scope, $location) {
     $scope.employees = [];
     $scope.employee = {};
     $scope.editing = false;
     $scope.formVisible = false;
 
-    $scope.showForm = function() {
-        $scope.formVisible = true;
-        $scope.editing = false;
-        $scope.employee = {};
-    }
-
-    $scope.submitForm = function() {
-        $scope.formSubmitted = true;
-        if (!$scope.employee.gender) {
-            return; 
-        }
-
-        if ($scope.editing) {
-            var index = findEmployeeIndex($scope.employee.id);
-            if (index !== -1) {
-                $scope.employees[index] = angular.copy($scope.employee);
-                $scope.cancelForm(); 
-            }
-        } else {
-            $scope.employee.id = generateId();
-            $scope.employees.push(angular.copy($scope.employee));
-            $scope.cancelForm(); 
-            
-        }
-  
-        $location.path("/table");
-        console.log($scope.employees);
+    $scope.showForm = function () {
+      $scope.formVisible = true;
+      $scope.editing = false;
+      $scope.employee = {};
     };
 
-    $scope.editEmployee = function(employee) {
-        $scope.employee = angular.copy(employee); 
-        $scope.editing = true;
-        $scope.formVisible = true;
-        //$location.path("/");
-    };
+    $scope.submitForm = function () {
+      $scope.formSubmitted = true;
+      if (!$scope.employee.gender) {
+        return;
+      }
 
-    $scope.deleteEmployee = function(employee) {
-        var index = findEmployeeIndex(employee.id);
+      if ($scope.editing) {
+        var index = findEmployeeIndex($scope.employee.id);
         if (index !== -1) {
-            $scope.employees.splice(index, 1);
+          $scope.employees[index] = angular.copy($scope.employee);
+          $scope.cancelForm();
         }
+      } else {
+        $scope.employee.id = generateId();
+        $scope.employees.push(angular.copy($scope.employee));
+        $scope.cancelForm();
+      }
+
+      $location.path("/table");
+      console.log($scope.employees);
     };
 
-    $scope.cancelForm = function() {
-        $scope.employee = {};
-        $scope.editing = false;
-        $scope.formVisible = true;
+    $scope.editEmployee = function (employee) {
+      $scope.employee = angular.copy(employee);
+      $scope.editing = true;
+      $scope.formVisible = true;
+      //$location.path("/");
+    };
+
+    $scope.deleteEmployee = function (employee) {
+      var index = findEmployeeIndex(employee.id);
+      if (index !== -1) {
+        $scope.employees.splice(index, 1);
+      }
+    };
+
+    $scope.cancelForm = function () {
+      $scope.employee = {};
+      $scope.editing = false;
+      $scope.formVisible = true;
     };
 
     function generateId() {
-        return $scope.employees.length + 1;
+      return $scope.employees.length + 1;
     }
 
     function findEmployeeIndex(id) {
-        for (var i = 0; i < $scope.employees.length; i++) {
-            if ($scope.employees[i].id === id) {
-                return i;
-            }
+      for (var i = 0; i < $scope.employees.length; i++) {
+        if ($scope.employees[i].id === id) {
+          return i;
         }
-        return -1;
+      }
+      return -1;
     }
-
   });
-
-
